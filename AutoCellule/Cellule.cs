62e2619095426dc7f2b01a74796951c22c803eaa
Clement Bazile <clement.bazile@etud.u-picaride.fr>
2018-06-07 11:33:43 +0200

    Ajoutez des fichiers projet.

---
+++ AutoCellule/Cellule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCellule
+{
+    class Cellule
+    {
+        protected bool Etat = true;//etat de la cellule(true = blanc)
+
+        protected int CoordoX;//coordonnées de la cellule 
+
+        protected int CoordoY;
+
+        public Cellule()// par defaut
+        {
+            this.CoordoX = 0;
+            this.CoordoY = 0;
+        }
+
+        public Cellule(int x,int y)//placement de cellules blanches
+        {
+            this.CoordoX = x;
+            this.CoordoY = y;
+        }
+    }
+}
